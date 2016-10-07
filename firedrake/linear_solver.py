515273789a70a1f7da3b12a1849e98018dbb2654
Thomas Gibson <t.gibson15@imperial.ac.uk>
2016-10-07 14:24:09 +0100

    testing and action draft

--- firedrake/linear_solver.py
+++ firedrake/linear_solver.py
@@ -128,20 +128,20 @@ class LinearSolver(solving_utils.ParametersMixin):
         if len(self._W) > 1 and self.transpose_nullspace is not None:
             self.transpose_nullspace._apply(self._W.dof_dset.field_ises, transpose=True)
         if self.A.has_bcs:
-            if isinstance(self.A.a, slate.Tensor):
-                # Homogeneous bcs, don't need action.
-                b_bc = self._b
-                b_bc.assign(b)
-                for bc in self.A.bcs:
-                    bc.apply(b_bc)
-            else:
-                b_bc = self._b
-                # rhs = b - action(A, zero_function_with_bcs_applied)
-                b_bc.assign(b - self._Abcs)
-                # Now we need to apply the boundary conditions to the "RHS"
-                for bc in self.A.bcs:
-                    bc.apply(b_bc)
-                # don't want to write into b itself, because that would confuse user
+#            if isinstance(self.A.a, slate.Tensor):
+#                # Homogeneous bcs, don't need action.
+#                b_bc = self._b
+#                b_bc.assign(b)
+#                for bc in self.A.bcs:
+#                    bc.apply(b_bc)
+#            else:
+            b_bc = self._b
+            # rhs = b - action(A, zero_function_with_bcs_applied)
+            b_bc.assign(b - self._Abcs)
+            # Now we need to apply the boundary conditions to the "RHS"
+            for bc in self.A.bcs:
+                bc.apply(b_bc)
+            # don't want to write into b itself, because that would confuse user
             b = b_bc
         with b.dat.vec_ro as rhs:
             with x.dat.vec as solution:
